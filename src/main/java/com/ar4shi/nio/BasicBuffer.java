6f2dd59c53d12dca9720e99a789a6039d672d63f
jianghanh <jianghanh@yonyou.com>
2022-03-16 01:59:16 +0800

    update nio code

---
+++ src/main/java/com/ar4shi/nio/BasicBuffer.java
@@ -0,0 +1,31 @@
+package com.ar4shi.nio;
+
+import java.nio.IntBuffer;
+
+/**
+ * @author jianghan
+ * @date 2022-03-08 03:28
+ */
+public class BasicBuffer {
+
+    public static void main(String[] args) {
+
+        // 创建一个buffer 大小为5
+        IntBuffer intBuffer = IntBuffer.allocate(5);
+
+        // 向buffer中存放数据
+        for (int i = 0; i < intBuffer.capacity(); i++) {
+            intBuffer.put(i << 1);
+        }
+
+        //要想从buffer读取数据 ，先要将buffer读写切换
+        intBuffer.flip();
+        intBuffer.position(1);
+        intBuffer.limit(4);
+
+        while (intBuffer.hasRemaining()) {
+            System.out.println(intBuffer.get());
+        }
+    }
+
+}
